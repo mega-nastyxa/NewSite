20556345585f79318b04291ac8047ef023e3d5f3
Anastasia Kravchenko <142917747+mega-nastyxa@users.noreply.github.com>
2025-03-31 13:43:17 +0300

    new menu

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,29 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("menuButton").addEventListener("click", function() {
-        document.querySelector(".container__header").classList.toggle("open");
-    });
+const menuButton = document.getElementById('menuButton');
+const menuBar = document.getElementById('menuBar');
+let timeoutId;
+
+// Показать меню при наведении на кнопку
+menuButton.addEventListener('mouseenter', () => {
+    clearTimeout(timeoutId); // Очищаем таймер, если он установлен
+    menuBar.classList.add('visible');
+});
+
+// Скрыть меню, когда мышь покидает кнопку
+menuButton.addEventListener('mouseleave', () => {
+    timeoutId = setTimeout(() => {
+        menuBar.classList.remove('visible');
+    }, 300); // Задержка в 200 мс
+});
+
+// Показать меню, когда мышь над меню
+menuBar.addEventListener('mouseenter', () => {
+    clearTimeout(timeoutId); // Очищаем таймер, если он установлен
+    menuBar.classList.add('visible');
+});
+
+// Скрыть меню, когда мышь покидает меню
+menuBar.addEventListener('mouseleave', () => {
+    timeoutId = setTimeout(() => {
+        menuBar.classList.remove('visible');
+    }, 300); // Задержка в 200 мс
 });

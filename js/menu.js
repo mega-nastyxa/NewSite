9cf7817a67bf6738a75ba45e1a0d5064f21086a4
Anastasia Kravchenko <diva_dog@mail.ru>
2025-04-11 11:53:38 +0300

    new

--- js/menu.js
+++ js/menu.js
@@ -1,32 +1,32 @@
-const menuButton = document.getElementById('menuButton');
-const menuBar = document.getElementById('menuBar');
-let timeoutId;
+// const menuButton = document.getElementById('menuButton');
+// const menuBar = document.getElementById('menuBar');
+// let timeoutId;
 
-// Показать меню при наведении на кнопку
-menuButton.addEventListener('mouseenter', () => {
-    clearTimeout(timeoutId); // Очищаем таймер, если он установлен
-    menuBar.classList.add('visible');
-});
+// // Показать меню при наведении на кнопку
+// menuButton.addEventListener('mouseenter', () => {
+//     clearTimeout(timeoutId); // Очищаем таймер, если он установлен
+//     menuBar.classList.add('visible');
+// });
 
-// Скрыть меню, когда мышь покидает кнопку
-menuButton.addEventListener('mouseleave', () => {
-    timeoutId = setTimeout(() => {
-        menuBar.classList.remove('visible');
-    }, 300); // Задержка в 200 мс
-});
+// // Скрыть меню, когда мышь покидает кнопку
+// menuButton.addEventListener('mouseleave', () => {
+//     timeoutId = setTimeout(() => {
+//         menuBar.classList.remove('visible');
+//     }, 300); // Задержка в 200 мс
+// });
 
-// Показать меню, когда мышь над меню
-menuBar.addEventListener('mouseenter', () => {
-    clearTimeout(timeoutId); // Очищаем таймер, если он установлен
-    menuBar.classList.add('visible');
-});
+// // Показать меню, когда мышь над меню
+// menuBar.addEventListener('mouseenter', () => {
+//     clearTimeout(timeoutId); // Очищаем таймер, если он установлен
+//     menuBar.classList.add('visible');
+// });
 
-// Скрыть меню, когда мышь покидает меню
-menuBar.addEventListener('mouseleave', () => {
-    timeoutId = setTimeout(() => {
-        menuBar.classList.remove('visible');
-    }, 300); // Задержка в 200 мс
-});
+// // Скрыть меню, когда мышь покидает меню
+// menuBar.addEventListener('mouseleave', () => {
+//     timeoutId = setTimeout(() => {
+//         menuBar.classList.remove('visible');
+//     }, 300); // Задержка в 200 мс
+// });
 
 
 
